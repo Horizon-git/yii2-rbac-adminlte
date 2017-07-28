c8a0d94961284875d021518f70f03fd942c75cbe
arydeoblo <rizki.device@gmail.com>
2017-07-28 18:16:13 +0800

    fix route view

--- views/route/index.php
+++ views/route/index.php
@@ -1,8 +1,8 @@
 <?php
 
+use bahirul\yii2rbac\adminlte\RbacRouteAsset;
 use yii\helpers\Html;
 use yii\helpers\Json;
-use bahirul\yii2rbac\adminlte\RbacRouteAsset;
 
 RbacRouteAsset::register($this);
 
@@ -13,15 +13,25 @@ $this->title = Yii::t('yii2mod.rbac', 'Routes');
 $this->params['breadcrumbs'][] = $this->title;
 $this->render('/layouts/_sidebar');
 ?>
-<h1><?php echo Html::encode($this->title); ?></h1>
-<?php echo Html::a(Yii::t('yii2mod.rbac', 'Refresh'), ['refresh'], [
-    'class' => 'btn btn-primary',
-    'id' => 'btn-refresh',
-]); ?>
-<?php echo $this->render('../_dualListBox', [
-    'opts' => Json::htmlEncode([
-        'items' => $routes,
-    ]),
-    'assignUrl' => ['assign'],
-    'removeUrl' => ['remove'],
-]); ?>
+<div class="rbac-route">
+    <div class="box box-primary">
+		<div class="box-header with-border">
+    		<h4><?php echo Html::encode($this->title); ?></h4>
+    	</div>
+	    <div class="box-body">
+	    
+			<?php echo Html::a(Yii::t('yii2mod.rbac', 'Refresh'), ['refresh'], [
+                'class' => 'btn btn-primary',
+                'id' => 'btn-refresh',
+            ]); ?>
+			<?php echo $this->render('../_dualListBox', [
+                'opts' => Json::htmlEncode([
+                    'items' => $routes,
+                ]),
+                'assignUrl' => ['assign'],
+                'removeUrl' => ['remove'],
+            ]); ?>
+
+		</div>
+	</div>
+</div>
